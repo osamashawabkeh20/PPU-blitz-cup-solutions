5e1aabebe9494204e61ecfe544a28bc3fdbaafb8
Osama Shawabkeh <osamashawabkeh20@gmail.com>
2026-02-04 19:56:31 +0200

    Create Group Qualifications Round3 Problem B.cpp

---
+++ Group Qualifications Round3 Problem B.cpp
@@ -0,0 +1,42 @@
+/*
+      problem link: https://codeforces.com/contest/1720/problem/B
+*/
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define int long long
+const int inf = 3e18;
+
+using namespace std;
+
+void Solve()
+{
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    cout << a[n - 1] - a[0] + a[n - 2] - a[1] << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        Solve();
+
+    cerr << "Done!" << endl;
+
+    return 0;
+}
